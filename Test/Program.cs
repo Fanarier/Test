5465a402a7da10ec379f20ac5ae4dc1f60e9540c
KP_349 <KP_349@FANARIER>
2022-10-15 19:43:38 +0200

    Добавьте файлы проекта.

---
+++ Test/Program.cs
@@ -0,0 +1,54 @@
+﻿//4.57. Плотников Кирилл Андреевич 22-ИСП-2/2
+//Дано трехзначное число. Определить:а)входит ли в него цифра 6;б)входит ли в него цифра n.
+try
+{
+    {
+        Console.WriteLine("Введите числo");//a
+        int num = Convert.ToInt32(Console.ReadLine());
+    int n = 6;
+    bool flag = false;
+        while (num > 0)
+        {
+            int m = num % 10;
+            if (m == n)
+            {
+                flag = true;
+            }
+            num = num / 10;
+        }
+        if (flag)
+        {
+            Console.WriteLine("есть");
+        }
+        else
+        {
+            Console.WriteLine("нет");
+        }
+    }
+{
+    Console.WriteLine("Введите числo");//b
+    int num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("n=");
+    int n = Convert.ToInt32(Console.ReadLine());
+    bool flag = false;
+    while (num > 0)
+    {
+        int m = num % 10;
+        if (m == n)
+        {
+            flag = true;
+        }
+        num = num / 10;
+    }
+    if (flag)
+    {
+        Console.WriteLine("есть");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
+}
+catch (Exception ex)
+{ Console.WriteLine(ex.ToString()); }
